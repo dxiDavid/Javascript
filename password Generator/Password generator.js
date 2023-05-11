4dcb493d4d9ab4dd7069fec448a066001badcdb1
dxiDavid <davidgichuruofficial@gmail.com>
2023-05-11 22:56:08 +0300

    Made a GUI, pending form checks and validation

--- password Generator/Password generator.js
+++ password Generator/Password generator.js
@@ -1,51 +1,59 @@
+const generateBtn = document.getElementById("generate")
+const outputBox = document.querySelector(".output")
+const inputBox = document.getElementById("input")
+const form = document.querySelector('form');
 
-	
-	let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	let numbers = "0123456789";
-	let specialCharacters = "!@#$%^&*`,_-=|./?'";
-	let lettersLength = letters.length;
-	let numbersLength = numbers.length; 
-	let specialCharactersLength = specialCharacters.length;
+form.addEventListener("keydown", (e) => {
+	if(e.key === "Enter"){
+		e.preventDefault
+	}
+})
 
-	function generateLetters(numberOfLetters){
-		let generatedLetters = "";
-		for(var i = 0; i < numberOfLetters; i++){
-			generatedLetters += letters.charAt(Math.floor(Math.random() * lettersLength)) 
-		}
-		return generatedLetters;
-	}
-	function generateNumbers(numberOfNumbers){
-		let generatedNumbers = "";
-		for(var i = 0; i < numberOfNumbers; i++){
-			generatedNumbers += numbers.charAt(Math.floor(Math.random() * numbersLength))
-		}
-		return generatedNumbers;
-	}
-	function generateSpecialCharacters(numberOfSpecialCharacters){
-		let  generatedSpecialCharacters = "";
-		for(var i = 0; i < numberOfSpecialCharacters; i++){
-			generatedSpecialCharacters += specialCharacters.charAt(Math.floor(Math.random() * specialCharactersLength));	
-		}
-		return generatedSpecialCharacters;
-	}
-	function stringRandomizer(values){
-		let index = values.length, randomIndex;
-		while(index != 0){
-			randomIndex = Math.floor(Math.random() *index)
-			index--
-			[values[index], values[randomIndex]] = [values[randomIndex], values[index]];
-		}
-		return values
+
+generateBtn.addEventListener("click", (e) => {
+
+	if(e.key === "Enter"){
+		e.preventDefault
 	}
 
-	function generate(variable, typeOfCharacter, numberOfCharactersRequired){
-		variable = "";
-		for(var i = 0; i < numberOfCharactersRequired; i++){
-			variable += typeOfCharacter.charAt(Math.floor(Math.random() * typeOfCharacter.length));	
-		}
-		return variable;
-	}
+	e.preventDefault()
+	e.target.classList.add("push");
+  	setTimeout(() => {e.target.classList.remove("push")}, 100);
 
-let output = generateLetters(7) + generateNumbers(6) + generateSpecialCharacters(6)
-output.split("")
-console.log(stringRandomizer(output.split("")).join(""))
+		let newPassword = generatePassword(inputBox.value)
+		let sliceLength = Math.ceil(newPassword.length / 3)
+		let shuffledPassword = stringRandomizer(newPassword)
+		outputBox.textContent = shuffledPassword.slice(0, sliceLength).join("")
+	  
+})
+
+// generator functions
+
+const char = {
+	letters: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+	numbers: "0123456789",
+	symbols: "!@#$%^&*`,_-=|./?'"
+}
+
+function generatePassword(passwordLength){
+	const { letters, numbers, symbols } = char
+	let password = "";
+
+	for(var i = 0; i < Math.round(passwordLength); i++){
+		password += letters.charAt(Math.floor(Math.random() * letters.length)) 
+		password += numbers.charAt(Math.floor(Math.random() * numbers.length)) 
+		password += symbols.charAt(Math.floor(Math.random() * symbols.length)) 
+	}
+	return stringRandomizer(password.split(""));
+}
+
+function stringRandomizer(values){
+	let index = values.length, randomIndex;
+	while(index != 0){
+		randomIndex = Math.floor(Math.random() *index)
+		index--
+		[values[index], values[randomIndex]] = [values[randomIndex], values[index]];
+	}
+	return values
+}
+
--- password Generator/generator.js
+++
@@ -1,3 +0,0 @@
-//have default values for the password
-//take integer input value and assign it to a generator parameter. Input type number
-//use generate button to call function generate on click
